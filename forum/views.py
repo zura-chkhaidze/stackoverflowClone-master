9f30c2d5eba5fe93334e631b068697c30c33e66d
zura-chkhaidze <zura.chkhaidze444@gmail.com>
2023-02-01 02:27:22 +0400

    zura edits:Add answer form on question detail view

--- forum/views.py
+++ forum/views.py
@@ -56,12 +56,7 @@ class AnswerDetailView(DetailView):
     model = Question
 
    
-    def get(self, request, *args, **kwargs):
-        self.object: Question = self.get_object()
-        self.object.views += 1
-        self.object.save()
-        context = self.get_context_data(object=self.object)
-        return self.render_to_response(context)
+   
 
 
 # def create_question_view(request):
